9cefc553a6e7e70a140c575876c08676938c8143
DESKTOP-0ENLD38\ADMIN <tu.bui1911@hcmut.edu.vn>
2024-11-27 00:15:27 +0700

    EX7

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -153,52 +153,52 @@ void display7SEG(int num)
 	}
 }
 /* USER CODE END 0 */
-//const int MAX_LED = 4;
-//int index_led = 0;
-//int led_buffer[4] = {1, 2, 3, 4};
-//void update7SEG(int index)
-//{
-//	switch(index)
-//	{
-//	case 0:
-//			display7SEG(led_buffer[0]);
-//			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-//			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-//			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-//			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-//			break;
-//	case 1:
-//			display7SEG(led_buffer[1]);
-//			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-//			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-//			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-//			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-//			break;
-//	case 2:
-//			display7SEG(led_buffer[2]);
-//			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-//			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-//			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
-//			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-//			break;
-//	case 3:
-//			display7SEG(led_buffer[3]);
-//			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-//			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-//			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-//			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
-//			break;
-//	default:
-//			break;
-//	}
-//}
-//void updateClockBuffer(int hour, int minute)
-//{
-//	led_buffer[0] = hour/10;
-//	led_buffer[1] = hour%10;
-//	led_buffer[2] = minute/10;
-//	led_buffer[3] = minute%10;
-//}
+const int MAX_LED = 4;
+int index_led = 0;
+int led_buffer[4] = {1, 2, 3, 4};
+void update7SEG(int index)
+{
+	switch(index)
+	{
+	case 0:
+			display7SEG(led_buffer[0]);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			break;
+	case 1:
+			display7SEG(led_buffer[1]);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			break;
+	case 2:
+			display7SEG(led_buffer[2]);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			break;
+	case 3:
+			display7SEG(led_buffer[3]);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
+			break;
+	default:
+			break;
+	}
+}
+void updateClockBuffer(int hour, int minute)
+{
+	led_buffer[0] = hour/10;
+	led_buffer[1] = hour%10;
+	led_buffer[2] = minute/10;
+	led_buffer[3] = minute%10;
+}
 int timer0_counter = 0;
 int timer0_flag = 0;
 int TIMER_CYCLE = 10;
@@ -253,18 +253,35 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-//  int hour = 15;
-//  int minute = 5;
-//  int second = 60;
+  int hour = 15;
+  int minute = 5;
+  int second = 60;
   setTimer0(1000);
   while (1)
   {
     /* USER CODE END WHILE */
 	  if(timer0_flag == 1)
 	  {
-		  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-		  setTimer0(2000);
+		  second++;
+		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+		  setTimer0(1000);
+
 	  }
+	  if(second >= 60)
+	  {
+		  minute++;
+		  second = 0;
+	  }
+	  if(minute >= 60)
+	  {
+		  hour++;
+		  minute = 0;
+	  }
+	  if(hour >= 24)
+	  {
+		  hour = 0;
+	  }
+	  updateClockBuffer(hour, minute);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -392,12 +409,25 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-int status = 0;
+//int status = 0;
 int counter = 50;
-int dotcounter = 100;
+//int dotcounter = 100;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	timer_run();
+	if(index_led > 3)
+	{
+		index_led = 0;
+	}
+	if(counter > 0)
+	{
+		update7SEG(index_led++);
+		counter--;
+	}
+	if(counter <= 0)
+	{
+		counter = 50;
+	}
 }
 /* USER CODE END 4 */
 
